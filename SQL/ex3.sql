d94eeedf8da957625388e7b800f18ecad0af801c
hassdbouk <hdbouk@uwo.ca>
2025-11-14 10:57:36 -0500

    formatting the second statement according to ISO

--- SQL/ex3.sql
+++ SQL/ex3.sql
@@ -10,11 +10,16 @@ INSERT INTO Player (playerId, firstName, lastName, nationality, teamId)
 SELECT r.playerId, r.firstName, r.lastName, r.nationality, t.teamId
 FROM (
     SELECT 'PLY001' AS playerId, 'Erling' AS firstName, 'Haaland' AS lastName, 'Norway' AS nationality, 'Manchester City' AS teamName
-    UNION ALL SELECT 'PLY002','Kevin','De Bruyne','Belgium','Manchester City'
-    UNION ALL SELECT 'PLY003','Phil','Foden','England','Manchester City'
-    UNION ALL SELECT 'PLY010','Bukayo','Saka','England','Arsenal'
-    UNION ALL SELECT 'PLY011','Martin','Odegaard','Norway','Arsenal'
-    UNION ALL SELECT 'PLY020','Mohamed','Salah','Egypt','Liverpool'
+    UNION ALL 
+    SELECT 'PLY002','Kevin','De Bruyne','Belgium','Manchester City'
+    UNION ALL 
+    SELECT 'PLY003','Phil','Foden','England','Manchester City'
+    UNION ALL 
+    SELECT 'PLY010','Bukayo','Saka','England','Arsenal'
+    UNION ALL 
+    SELECT 'PLY011','Martin','Odegaard','Norway','Arsenal'
+    UNION ALL 
+    SELECT 'PLY020','Mohamed','Salah','Egypt','Liverpool'
 ) AS r
 JOIN Team AS t ON t.teamName = r.teamName;
 
