bdb02db137ff8a858383771c7943a607020a64d3
Ayden Bozzelli <abozzelli33@gmail.com>
2025-11-17 14:19:24 -0500

    Added in basic ideas for step 5

--- SQL/ex5.sql
+++ SQL/ex5.sql
@@ -1,4 +1,25 @@
+-- Q1 select team from a specific homecity (london)
 SELECT teamId, teamName, homeCity
 FROM Team
 WHERE homeCity = 'London';
 
+
+-- Q2: Positive Win Rate, or order by win rate percentage (Team + team stats + ranking?)
+
+-- Q3: Career Goals for a player (season + player + team)
+
+-- Q4: Players on a team (players + teams)
+
+-- Q5: Players and teams  affected by news (Player + teams + news)
+
+-- Q6: team goals per a season (team + season + match)
+
+-- Q7: Most goals in a location (location + match + teams)
+
+-- Q8: Most goals against a team x (team + match)
+
+-- Q9: Players on a fantasy team x (players + fantasy team)
+
+-- Q10: Players Trade History (player + trade) x
+
+-- Q11: 
