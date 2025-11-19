0ee09bb00d4a527ae2d3f81de7312b7d2657a490
hassdbouk <hdbouk@uwo.ca>
2025-11-19 10:15:30 -0500

    constructed the second view statement which isnt updatable

--- SQL/ex7.sql
+++ SQL/ex7.sql
@@ -12,6 +12,19 @@ VALUES ('P001561', 'Sergio', 'Ramos', 'DEFENDER', 'Spain', 'RMADRID');
 
 
 
+CREATE VIEW AdvertisementRevenue
+AS SELECT priority, COUNT(*) AS adCount, SUM(revenue) AS totalRevenue, AVG(revenue) AS avgRevenue
+   FROM Advertisement
+   GROUP BY priority;
+   
+SELECT priority, adCount, ROUND(totalRevenue, 2) AS totalRevenue, ROUND(avgRevenue, 2) AS avgRevenue
+FROM AdvertisementRevenue;
+
+INSERT INTO AdvertisementRevenue (priority, adCount, totalRevenue, avgRevenue)
+VALUES (3, 15, 5000, 545); 
+
+   
+
 
 
    
